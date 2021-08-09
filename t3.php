fe420031dd91d9094b5b66e1650a836a23f7cbe5
Yiying Chen <yiying.chen@ericsson.com>
2021-08-09 16:18:42 +0800

    dev2: one-submit fix bug in t3.php

--- t3.php
+++ t3.php
@@ -3,6 +3,7 @@
 //this is t3.php
 echo "t3.php";
 echo "2+ 3 = ?";
+echo "Fix bug in dev2";
 
 //relative for master: t1 change
 //another line for master
--- t4.php
+++ t4.php
@@ -1,2 +1,3 @@
 <?php
 //dev init for t4.php 
+echo 1,2,3; //dev2
